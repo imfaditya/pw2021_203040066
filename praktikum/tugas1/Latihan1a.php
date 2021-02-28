2dbcca82ea4d845a662db0fc7e8ad8995575dfdc
imfaditya <imamaditya2211@gmail.com>
2021-02-28 12:08:48 +0700

    Upload praktikum tugas 1

--- praktikum/tugas1/Latihan1a.php
+++ praktikum/tugas1/Latihan1a.php
@@ -3,20 +3,10 @@
 // 203040066
 // Jumat 10.00 - 11.00
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Latihan 1a 203040066</title>
-</head>
-<body>
-        <?php for ($i=1; $i<=3; $i++){
-            for ($y=1; $y<=3; $y++){
-                echo "Ini perulangan ke ($i,$y)<br>";
-            }
-        } ?>
-        <!-- 066 -->
-</body>
-</html>
+<?php for ($i=1; $i<=3; $i++){
+    for ($y=1; $y<=3; $y++){
+        echo "Ini perulangan ke ($i,$y)<br>";
+    }
+}
+// 066 
+?>
