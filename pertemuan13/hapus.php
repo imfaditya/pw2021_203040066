982c247f7ca41dbf9c97a74ddb5db05cac2d99ff
imfaditya <imamaditya2211@gmail.com>
2021-05-15 06:59:24 +0700

    Upload tugas pertemuan 13

---
+++ pertemuan13/hapus.php
@@ -0,0 +1,35 @@
+<?php
+// Imam Faraz Aditya
+// 203040066
+// github.com/imfaditya/pw2021_203040066
+// Pertemuan 13 (04-04-2021)
+// Materi pertemuan 13 adalah ajax live search dan upload gambar
+?>
+<?php
+session_start();
+
+if (!isset($_SESSION['login'])) {
+  header("Location: login.php");
+  exit;
+}
+
+require 'functions.php';
+
+// Jika tidak ada id di url
+if (!isset($_GET['id'])) {
+  header("Location: index.php");
+  exit;
+}
+
+//Mengambil id dari url
+$id = $_GET['id'];
+
+if (hapus($id) > 0) {
+  echo "<script>
+            alert('Data Berhasil Dihapus');
+            document.location.href ='index.php';
+          </script>";
+} else {
+  echo "Data Gagal Dihapus";
+}
+?>
---
+++ pertemuan13/logout.php
@@ -0,0 +1,13 @@
+<?php
+// Imam Faraz Aditya
+// 203040066
+// github.com/imfaditya/pw2021_203040066
+// Pertemuan 13 (04-04-2021)
+// Materi pertemuan 13 adalah ajax live search dan upload gambar
+?>
+<?php
+session_start();
+session_destroy();
+header("Location:login.php");
+exit;
+?>
